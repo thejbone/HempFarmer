77f0aa057169bc17f7f827245313a69b1166cc0c
Thomas Rohloff <v10lator@myway.de>
2018-10-31 02:45:35 +0100

    Rewrite drop handling
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- 1.12_1.12.2/src/main/java/com/shruglabs/hempfarmer/block/cannibis/Hemp.java
+++ 1.12_1.12.2/src/main/java/com/shruglabs/hempfarmer/block/cannibis/Hemp.java
@@ -1,8 +1,6 @@
 package com.shruglabs.hempfarmer.block.cannibis;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import com.shruglabs.hempfarmer.ConfigHandler;
 import com.shruglabs.hempfarmer.block.HFBlockCrops;
@@ -13,10 +11,10 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public class Hemp extends HFBlockCrops {
 
@@ -54,30 +52,28 @@ public class Hemp extends HFBlockCrops {
 	}
 
 	@Override
-	public java.util.List<ItemStack> getDrops(net.minecraft.world.IBlockAccess world, BlockPos pos, IBlockState state,
+	public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state,
 			int fortune) {
-		super.getDrops(world, pos, state, fortune);
-		List<ItemStack> ret = new ArrayList<ItemStack>();
+		NonNullList<ItemStack> ret = NonNullList.create();
 		int age = getAge(state);
-		Random rand = world instanceof World ? ((World) world).rand : HUtils.random;
-
+		int i;
 		if (age >= getMaxAge()) {
-			if (rand.nextInt(2 * getMaxAge()) <= age) {
-				ret.add(new ItemStack(this.getSeed(), ConfigHandler.hempSeedsCropAmount));
-				if (this.getCropName() == "bud") {
-					ret.add(new ItemStack(this.getCrop(), ConfigHandler.hempBudAmount));
-				} else {
-					ret.add(new ItemStack(this.getCrop(), ConfigHandler.hempAmount));
-				}
-
-			}
+			i = ConfigHandler.hempSeedsCropAmount - 3;
+			if(i > 0)
+				super.getDrops(ret, world, pos, state, i);
+			Item crop = getCrop();
+			i = HUtils.random.nextInt((crop == HFItems.raw_hemp ? ConfigHandler.hempAmount : ConfigHandler.hempBudAmount) + 1);
+			if (i > 0)
+				ret.add(new ItemStack(crop, i));
 		}
-		if (ConfigHandler.enableWand) {
-			int x = rand.nextInt(100) + 1;
-			if (x > 99) {
-				ret.add(new ItemStack(HFItems.leaf, rand.nextInt(2) + 1));
-			}
+		
+		if (ConfigHandler.enableWand && HUtils.random.nextBoolean())
+		{
+			i = HUtils.random.nextInt(3);
+			if (i > 0)
+				ret.add(new ItemStack(HFItems.leaf, i));
 		}
+		
 		return ret;
 	}
 
