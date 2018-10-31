af7baf93ff22dbf56f69fefa85cf8097c8616f3d
Thomas Rohloff <v10lator@myway.de>
2018-10-31 02:58:11 +0100

    Rework joint smoking
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- 1.12_1.12.2/src/main/java/com/shruglabs/hempfarmer/item/joint/IndicaJoint.java
+++ 1.12_1.12.2/src/main/java/com/shruglabs/hempfarmer/item/joint/IndicaJoint.java
@@ -18,14 +18,7 @@ public class IndicaJoint extends Joint {
 	@Override
 	@Nullable
 	public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entity) {
-		stack.shrink(1);
-		entity.addPotionEffect(new PotionEffect(Potion.getPotionById(2), 400, 1, true, false));
-		entity.addPotionEffect(new PotionEffect(Potion.getPotionById(8), 400, 2, true, false));
-		entity.addPotionEffect(new PotionEffect(Potion.getPotionById(9), 50, 3, true, false));
 		entity.addPotionEffect(new PotionEffect(Potion.getPotionById(16), 200, 2, true, false));
-		entity.addPotionEffect(new PotionEffect(Potion.getPotionById(17), 150, 1, true, false));
-		entity.addPotionEffect(new PotionEffect(Potion.getPotionById(24), 400, 2, true, false));
-		smoke(entity, world);
-		return stack;
+		return super.onItemUseFinish(stack, world, entity);
 	}
 }
